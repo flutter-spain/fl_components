5a50c0cde1a6357342ddfef1aeb302c9dfca7500
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2022-12-25 00:45:16 +0100

    91 Widget Form

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -7,59 +7,93 @@ class InputsScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'first_name': 'Marlon',
+      'last_name': 'Falcon',
+      'email': 'demo@demo.cl',
+      'password': '123',
+      'role' : 'Admin'
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Text Inputs'),
       ),
       body:  SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-        child: Column(
-          children: const [
-              
-              customInputField( 
-                    labelText: 'Nombre',
-                    hintText: 'Ingrese su nombre',
-                    helperText: 'Solo letras',
-                    icon: Icons.person,
-                    suffixIcon: Icons.accessibility_new,
-                    obscureText: false,
-                  ),
-          
-              SizedBox(height: 20),
-              
+        child: Form(
+          key: myFormKey,
 
-              customInputField( 
-                    labelText: 'Apellidos',
-                    hintText: 'Ingrese sus apellidos',
-                    helperText: 'Solo letras',
-                    icon: Icons.person,
-                    suffixIcon: Icons.accessibility_new,
-                    obscureText: false,
-                  ),
+          child: Column(
+            children:  [
+                
+                const customInputField( 
+                      labelText: 'Nombre',
+                      hintText: 'Ingrese su nombre',
+                      helperText: 'Solo letras',
+                      icon: Icons.person,
+                      suffixIcon: Icons.accessibility_new,
+                      obscureText: false,
+                    ),
+            
+                SizedBox(height: 20),
+                
+        
+                const customInputField( 
+                      labelText: 'Apellidos',
+                      hintText: 'Ingrese sus apellidos',
+                      helperText: 'Solo letras',
+                      icon: Icons.person,
+                      suffixIcon: Icons.accessibility_new,
+                      obscureText: false,
+                    ),
+        
+                SizedBox(height: 20),
+        
+                const customInputField( 
+                  labelText: 'Email',
+                  hintText: 'Ingrese su email',
+                  helperText: 'Solo letras',
+                  icon: Icons.email,
+                  suffixIcon: Icons.email,
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: false,
+                ),
+        
+                SizedBox(height: 20),
+                const customInputField( 
+                  labelText: 'Password',
+                  hintText: 'Ingrese su password',
+                  helperText: 'Solo letras',
+                  icon: Icons.lock,
+                  suffixIcon: Icons.lock_open,
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                ),
+                const SizedBox(height: 20),
+        
+                ElevatedButton(
+                  onPressed: () {
+                    FocusScope.of(context).requestFocus( FocusNode() );
 
-              SizedBox(height: 20),
+                    if ( !myFormKey.currentState!.validate() ) {
+                      print("Formulario no valido");
+                      return;
+                    }
 
-              customInputField( 
-                labelText: 'Email',
-                hintText: 'Ingrese su email',
-                helperText: 'Solo letras',
-                icon: Icons.email,
-                suffixIcon: Icons.email,
-                keyboardType: TextInputType.emailAddress,
-                obscureText: false,
-              ),
-
-              SizedBox(height: 20),
-              customInputField( 
-                labelText: 'Password',
-                hintText: 'Ingrese su password',
-                helperText: 'Solo letras',
-                icon: Icons.lock,
-                suffixIcon: Icons.lock_open,
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-              ),
-          ],
+                    // Imprimir los valores del formulario
+                    print('guardar');
+                    print(formValues);
+                  }, 
+                  child: const SizedBox(
+                    width: double.infinity,
+                    child: Center(child: Text('Guardar'))
+                  ))
+            ],
+          ),
         ),
 
       )
