159ec0e178c5c0e6336018861eb5914232127d5d
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2022-12-24 13:03:38 +0100

    69 Diseño del Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,10 +6,23 @@ class HomeScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Componentes en Flutter'),
+        elevation: 0,
       ),
+      body: ListView.separated(
+        itemBuilder: (BuildContext context, int index) => ListTile(
+          title: const Text('ListView Tipo 1'),
+          leading: Icon(Icons.list, color: Colors.indigo),
+          subtitle: const Text('Pulse para entrar'),
+          trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.indigo),
+          onTap: () {
+            Navigator.pushNamed(context, 'listview1');
+          },
+        ),
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+        itemCount: 5)
     );
   }
 }
