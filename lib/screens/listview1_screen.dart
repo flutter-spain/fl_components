ee638d47931810c8f9d964e51948ea4bd8b878c0
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2022-12-24 12:10:07 +0100

    65

--- lib/screens/listview1_screen.dart
+++ lib/screens/listview1_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ListView1Screen extends StatelessWidget {
+
+  final options = const['Megaman', 'Metal Gear', 'Castlevania', 'Metroid', 'Chrono Trigger'];
    
   const ListView1Screen({Key? key}) : super(key: key);
   
@@ -11,10 +13,14 @@ class ListView1Screen extends StatelessWidget {
         title: const Text('ListView Tipo 1'),
       ),
       body: ListView(
-        children: const [
-          Text('Hola Mundo'),
-          Text('Hola Mundo'),
-          Text('Hola Mundo'),
+        children:  [
+          ...options.map(
+            (e) => ListTile(
+              title: Text(e),
+              subtitle: const Text('Pulse para entrar'),
+              trailing: const Icon(Icons.keyboard_arrow_right),
+            )
+          ).toList()
         ]
       )
     );
