0faed9c9399e1ac4b32f1078a4c49d4594ec5f60
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2022-12-25 02:39:03 +0100

    97 Infinite

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
 
-class ListViewBuilderScreen extends StatelessWidget {
+class ListViewBuilderScreen extends StatefulWidget {
    
   const ListViewBuilderScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<ListViewBuilderScreen> createState() => _ListViewBuilderScreenState();
+}
+
+class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
+
+  final List<int> imagesIds = [1,2,3,4,5,6,7,8,9,10];
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() 
+       {
+         print('${scrollController.position.pixels}, ${scrollController.position.maxScrollExtent}');
+          if (scrollController.position.pixels + 500 >= scrollController.position.maxScrollExtent) {
+            add5();
+          }
+      }
+    );
+  }
+
+  void add5(){
+    final lastId = imagesIds.last;
+    for (var i = 1; i < 5; i++) {
+      imagesIds.add(lastId + i);
+    }
+    setState(() {});
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -14,14 +45,15 @@ class ListViewBuilderScreen extends StatelessWidget {
 
         
         child: ListView.builder(
-          itemCount: 20,
+          controller: scrollController,
+          itemCount: imagesIds.length,
           itemBuilder: (BuildContext context, int index) {
             return FadeInImage(
               width: double.infinity,
               height: 300,
               fit: BoxFit.cover,
               placeholder: const AssetImage('assets/img/jar-loading.gif'),
-              image: NetworkImage('https://picsum.photos/500/300?random=${index + 1}'),
+              image: NetworkImage('https://picsum.photos/500/300?random=${imagesIds[index]}'),
             );
           },
         ),
