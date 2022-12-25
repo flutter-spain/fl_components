c1c3625cd878ac5d25703b31446eabb3524b5588
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2022-12-25 01:39:08 +0100

    95 Listas

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -12,6 +12,7 @@ class SliderScreen extends StatefulWidget {
 class _SliderScreenState extends State<SliderScreen> {
 
   double _sliderValue = 100;
+  bool _checkboxValue = true;
 
   @override
   Widget build(BuildContext context) {
@@ -29,17 +30,62 @@ class _SliderScreenState extends State<SliderScreen> {
                 max: 200,
                 activeColor: AppThemeIndigo.primaryColor,
                 label: '10',
-                onChanged: (value) {
+                onChanged: _checkboxValue
+                ? (value) {
                   _sliderValue = value;
+                  setState(() {});
+                  print(value);
+                }
+                : null,
+              ),
+
+              Checkbox(
+                value: _checkboxValue,
+                onChanged: (value) {
+                  print(value);
+                  _checkboxValue = value ?? true;
                   setState(() {
 
                   });
-                  print(value);
                 },
               ),
 
-              Image(image: NetworkImage('https://img.fruugo.com/product/3/69/131338693_max.jpg'),
-                    width: _sliderValue * 3,)
+              CheckboxListTile(
+                title: const Text('Habilitar Slider'),
+                value: _checkboxValue,
+                activeColor: AppThemeIndigo.primaryColor,
+                onChanged: (value) {
+                  print(value);
+                  _checkboxValue = value ?? true;
+                  setState(() {
+
+                  });
+                },
+              ),
+
+              SwitchListTile(
+                title: const Text('Habilitar Slider'),
+                value: _checkboxValue,
+                activeColor: AppThemeIndigo.primaryColor,
+                onChanged: (value) {
+                  print(value);
+                  _checkboxValue = value ?? true;
+                  setState(() {
+
+                  });
+                },
+              ),
+              
+
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Image(image: NetworkImage('https://img.fruugo.com/product/3/69/131338693_max.jpg'),
+                        width: _sliderValue * 5,),
+                ),
+              ),
+
+              
+
 
           ],
       )
